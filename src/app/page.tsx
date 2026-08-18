d32dcc7f4bf6a1d12fd9c35164ba7a1d0c91ee87
Isaac <isaacfloyd205@gmail.com>
2026-08-18 15:12:05 -0400

    Route v2 deployment root to v2 experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import EvaluationPage from "./EvaluationPage";
+import V2EvaluationPage from "./V2EvaluationPage";
 
 export default function Home() {
+  if (process.env.NEXT_PUBLIC_APP_VERSION === "v2") {
+    return <V2EvaluationPage />;
+  }
+
   return <EvaluationPage />;
 }
